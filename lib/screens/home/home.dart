521eb5dc24fe8dfa819786a0a093f81ce055650b
GanyUwU <91002859+GanyUwU@users.noreply.github.com>
2024-10-04 13:40:41 +0530

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -190,6 +190,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               SizedBox(height: 20),
+              Text("hello there");
 
              // REQUESTS
 
